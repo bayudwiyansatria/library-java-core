cb51fd4ac7d0643c8f4e0c077804f6c9937cf796
Bayu Dwiyan Satria <bayudwiyansatria@gmail.com>
2019-09-28 17:47:48 +0700

    Cleanning

--- src/main/java/com/bayudwiyansatria/Core.java
+++
@@ -1,7 +0,0 @@
-package com.bayudwiyansatria;
-
-public class Core {
-    public String getUsername(){
-        return System.getProperty("user.name");
-    }
-}
--- src/main/java/com/bayudwiyansatria/utils/Plot2D.java
+++
@@ -1,7 +0,0 @@
-package com.bayudwiyansatria.utils;
-
-import com.bayudwiyansatria.mat.Array;
-
-public class Plot2D extends Array {
-
-}
