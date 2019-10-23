1f7d29c73ac76c48a48fd9b40b669f825ac98404
Bayu Dwiyan Satria <bayudwiyansatria@gmail.com>
2019-10-23 19:59:37 +0700

    Staging adn Add Test

---
+++ src/test/java/com/bayudwiyansatria/mat/VectorTest.java
@@ -0,0 +1,173 @@
+package com.bayudwiyansatria.mat;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class VectorTest {
+	
+	@Test
+	public void getDistanceAbsolute ( ) {
+	}
+	
+	@Test
+	public void testGetDistanceAbsolute ( ) {
+	}
+	
+	@Test
+	public void testGetDistanceAbsolute1 ( ) {
+	}
+	
+	@Test
+	public void getDistanceRelative ( ) {
+	}
+	
+	@Test
+	public void testGetDistanceRelative ( ) {
+	}
+	
+	@Test
+	public void testGetDistanceRelative1 ( ) {
+	}
+	
+	@Test
+	public void testGetDistanceRelative2 ( ) {
+	}
+	
+	@Test
+	public void getDistance ( ) {
+	}
+	
+	@Test
+	public void testGetDistance ( ) {
+	}
+	
+	@Test
+	public void testGetDistance1 ( ) {
+	}
+	
+	@Test
+	public void testGetDistance2 ( ) {
+	}
+	
+	@Test
+	public void testGetDistanceAbsolute2 ( ) {
+	}
+	
+	@Test
+	public void testGetDistanceAbsolute3 ( ) {
+	}
+	
+	@Test
+	public void testGetDistanceAbsolute4 ( ) {
+	}
+	
+	@Test
+	public void testGetDistanceAbsolute5 ( ) {
+	}
+	
+	@Test
+	public void testGetDistanceAbsolute6 ( ) {
+	}
+	
+	@Test
+	public void testGetDistanceRelative3 ( ) {
+	}
+	
+	@Test
+	public void testGetDistanceRelative4 ( ) {
+	}
+	
+	@Test
+	public void testGetDistanceRelative5 ( ) {
+	}
+	
+	@Test
+	public void testGetDistanceRelative6 ( ) {
+	}
+	
+	@Test
+	public void testGetDistance3 ( ) {
+	}
+	
+	@Test
+	public void testGetDistance4 ( ) {
+	}
+	
+	@Test
+	public void testGetDistance5 ( ) {
+	}
+	
+	@Test
+	public void testGetDistance6 ( ) {
+	}
+	
+	@Test
+	public void getMinDistanceRelative ( ) {
+	}
+	
+	@Test
+	public void testGetMinDistanceRelative ( ) {
+	}
+	
+	@Test
+	public void testGetMinDistanceRelative1 ( ) {
+	}
+	
+	@Test
+	public void getMinDistanceAbsolute ( ) {
+	}
+	
+	@Test
+	public void testGetMinDistanceAbsolute ( ) {
+	}
+	
+	@Test
+	public void testGetMinDistanceAbsolute1 ( ) {
+	}
+	
+	@Test
+	public void transposeMatrix ( ) {
+	}
+	
+	@Test
+	public void testTransposeMatrix ( ) {
+	}
+	
+	@Test
+	public void testTransposeMatrix1 ( ) {
+	}
+	
+	@Test
+	public void reshapeMatrix ( ) {
+	}
+	
+	@Test
+	public void testReshapeMatrix ( ) {
+	}
+	
+	@Test
+	public void testReshapeMatrix1 ( ) {
+	}
+	
+	@Test
+	public void testReshapeMatrix2 ( ) {
+	}
+	
+	@Test
+	public void testReshapeMatrix3 ( ) {
+	}
+	
+	@Test
+	public void testReshapeMatrix4 ( ) {
+	}
+	
+	@Test
+	public void getDistanceMetric ( ) {
+	}
+	
+	@Test
+	public void testGetDistanceMetric ( ) {
+	}
+	
+}
---
+++ src/test/java/com/bayudwiyansatria/utils/UtilsTest.java
@@ -0,0 +1,16 @@
+package com.bayudwiyansatria.utils;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UtilsTest {
+	
+	@Test
+	public void warning() {
+	}
+	
+	@Test
+	public void int_to_double() {
+	}
+}
